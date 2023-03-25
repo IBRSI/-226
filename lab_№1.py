ef7381f68f7d4c113b0bed99f79a8dae5ed8c8f1
IBRSI <128932021+IBRSI@users.noreply.github.com>
2023-03-26 00:46:52 +0500

    Add files via upload

---
+++ lab_№1.py
@@ -0,0 +1,45 @@
+# 1
+'''a = [1, 2, 3, 4, 5]
+print(a[0], a[2], a[-2])'''
+
+# 2
+'''a=[1,2,3,4,5,6,7,8,9]
+N=int(input())
+if N<=len(a):
+    print(a[N]**N)
+else:
+    print(-1)'''
+
+# 3
+'''a='pflfybt'
+s='f'
+k=0
+for i in range (0, len(a)-1):
+    if a[i]==s:
+        k+=1
+    if k==2:
+        print(i)
+        break'''
+
+# 4
+'''num = '00100111000000'
+num = num[::-1]
+print(num)
+c = 0
+for n in num:
+    if n == '0':
+        c += 1
+    else:
+        break
+print(c)'''
+
+# 6
+'''a = input()
+c = False
+for i in range(0, len(a)-1):
+    if b[i] == b[i+1]:
+        c = True
+    else:
+        c = False
+        break
+print(c)'''
