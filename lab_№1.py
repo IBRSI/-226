239f49ffb025d53c867d7bba9abd91944bafab5b
IBRSI <128932021+IBRSI@users.noreply.github.com>
2023-03-26 20:27:05 +0500

    Update lab_№1.py

--- lab_№1.py
+++ lab_№1.py
@@ -14,7 +14,7 @@ else:
 '''a='pflfybt'
 s='f'
 k=0
-for i in range (0, len(a)-1):
+for i in range (0, len(a)):
     if a[i]==s:
         k+=1
     if k==2:
@@ -47,3 +47,53 @@ for i in range(0, len(a)-1):
         c = False
         break
 print(c)'''
+
+#7
+'''p = str(input())
+while len(p) < 16:
+    p = input()
+a, b, c, d = False, False, False, True
+for m in p:
+    if m.isupper():
+        a = True
+    elif m.islower():
+        b = True
+    elif m.isnumeric():
+        c = True
+    else:
+        d = False
+if a and b and c and d:
+    print('Безопасный пароль')
+else:
+    print('Небезопасный пароль')'''
+
+#8
+'''def F(d):
+    for i in d:
+        if type(i) is list:
+            F(i)
+        else:
+            print(i,end=' ')
+    return
+n=[1,[2,3],[4,[5,6]]]
+F(n)'''
+
+#9
+'''a = {'Один': 1,'Четыре': 4, 'Два': 2, 'Три': 3}
+max=0
+for k, с in a.items():
+    if с > max:
+        max=с
+        key=k
+print(key)'''
+
+
+#10
+'''def F(a):
+    X = []
+    for i in a:
+        if a.count(i)>1:
+            X.append(i)
+    return X
+
+print(F([1,2,2,2,5,8,1]))'''
