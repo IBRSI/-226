143753d435cbddc584a57cd19638c9c0f208a0f2
IBRSI <128932021+IBRSI@users.noreply.github.com>
2023-04-13 19:55:52 +0500

    Update lab_№2.py

--- lab_№2.py
+++ lab_№2.py
@@ -53,7 +53,7 @@ n = [1, [2, [3,4]]]
 print( nestsum(n))
 
 #5
-ddef sequence(lst):
+def sequence(lst):
     if len(lst) == 1:
         return lst
     temp, save = [], []
@@ -83,18 +83,20 @@ text = 'чмаф всех в чатике'
 function(text)
 
 #7
-n = int(input())
-w = n*2-1
-a = [ ]
-
-for y in range(w):
-   a.append([])
-   for x in range(w):
-       d = n - abs(x+1-n) - abs(y+1-n)
-       a[y].append( '*' if d > 0 else " ")
-
-for l in a:
-    print(*l,sep='')
+n=int(input())
+for i in range(0,n+1):
+    for j in range(0,n-i):
+        print(end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+if i==n:
+    for i in range(n-1,0,-1):
+        for j in range(0,n-i):
+            print(end=" ")
+        for j in range(0,i):
+            print("*",end=" ")
+        print()
 
 #8
 def matrix(num):
